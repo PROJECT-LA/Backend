833f1e08be420cc5bbfe209a1936d08194c68086
Limberg Montes <limberg.montes@agetic.gob.bo>
2024-04-27 15:56:38 -0400

    feat: se adiciono el logout

--- src/core/auth/controller/auth.controller.ts
+++ src/core/auth/controller/auth.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Post, Request, Res, UseGuards } from '@nestjs/common'
+import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common'
 import { LocalAuthGuard } from '../guards/local-auth.guard'
-import { Response } from 'express'
 import { AuthService } from '../service'
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger'
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger'
 import { AuthDto } from '../dto'
 import { CurrentUser } from '../decorators/current-user.decorator'
 import { User } from 'src/core/users'
-
+import { JwtAuthGuard } from '../guards/jwt-auth.guard'
+import { Request, Response } from 'express'
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
@@ -35,4 +35,12 @@ export class AuthController {
   async validateUser(@CurrentUser() user: User) {
     return user
   } */
+  @ApiOperation({ summary: 'API para logout' })
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Get('logout')
+  async logout(@Req() req: Request, @Res() res: Response) {
+    res.clearCookie('jid')
+    return res.status(200).json()
+  }
 }
