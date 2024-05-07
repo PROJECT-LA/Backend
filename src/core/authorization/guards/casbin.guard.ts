5adc86aaa6664bd3e72790ffc6c4d627303e8d90
Limberg Montes <limberg.montes@agetic.gob.bo>
2024-05-07 11:10:26 -0400

    refactor: se modifico el acceso medinate roles del guard

--- src/core/authorization/guards/casbin.guard.ts
+++ src/core/authorization/guards/casbin.guard.ts
@@ -9,7 +9,7 @@ import {
 import { AUTHZ_ENFORCER } from 'nest-authz'
 import { Request } from 'express'
 import { Enforcer } from 'casbin/lib/cjs/enforcer'
-import { User } from 'src/core/users'
+import { PassportUser } from 'src/common'
 
 @Injectable()
 export class CasbinGuard implements CanActivate {
@@ -23,20 +23,20 @@ export class CasbinGuard implements CanActivate {
       method: action,
     } = context.switchToHttp().getRequest() as Request
     const resource = Object.keys(query).length ? route.path : originalUrl
-    const user = context.switchToHttp().getRequest().user as User
+    const user = context.switchToHttp().getRequest().user as PassportUser
     if (!user) {
       throw new UnauthorizedException()
     }
 
-    const roleNames = user.roles.map((role) => role.name)
-
-    let isPermitted = false
-    for (const role of roleNames) {
-      isPermitted = await this.enforcer.enforce(role, resource, action)
-      if (isPermitted) {
-        break
-      }
+    if (!user.roleName || !user.idRole) {
+      throw new UnauthorizedException()
     }
+
+    const isPermitted = await this.enforcer.enforce(
+      user.roleName,
+      resource,
+      action
+    )
 
     if (isPermitted) {
       return true
